cc42a81b7bf964be95e09f35818f3b27ee7882f8
priyamita <priyamita@student.telkomuniversity.ac.id>
2025-06-19 20:44:51 +0700

    Initial commit for layanan pengaduan

---
+++ WP-main/resources/views/template/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAPA - Sahabat Perempuan dan Anak</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    @include('template.nav')
+    @yield('content_template')
+    @include('template.footer')
+</body>
+
+</html>
